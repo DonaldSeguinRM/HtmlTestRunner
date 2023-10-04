3388c7271dbe2a91e26d52ab008907c64bf55650
Donald Seguin <dseguin@ruggedmonitoring.com>
2023-10-04 17:56:43 -0400

    Fix compatibility issue with Python 3.10

--- HtmlTestRunner/result.py
+++ HtmlTestRunner/result.py
@@ -12,6 +12,8 @@ from jinja2 import Template
 
 
 DEFAULT_TEMPLATE = os.path.join(os.path.dirname(__file__), "template", "report_template.html")
+STDOUT_LINE = '\nStdout:\n%s'
+STDERR_LINE = '\nStderr:\n%s'
 
 
 def load_template(template):
@@ -404,47 +406,23 @@ class HtmlTestResult(TextTestResult):
             report_file.write(report)
 
     def _exc_info_to_string(self, err, test):
-        """ Converts a sys.exc_info()-style tuple of values into a string."""
-        # if six.PY3:
-        #     # It works fine in python 3
-        #     try:
-        #         return super(_HTMLTestResult, self)._exc_info_to_string(
-        #             err, test)
-        #     except AttributeError:
-        #         # We keep going using the legacy python <= 2 way
-        #         pass
-
-        # This comes directly from python2 unittest
+        """Converts a sys.exc_info()-style tuple of values into a string."""
         exctype, value, tb = err
-        # Skip test runner traceback levels
-        while tb and self._is_relevant_tb_level(tb):
-            tb = tb.tb_next
-
-        if exctype is test.failureException:
-            # Skip assert*() traceback levels
-            length = self._count_relevant_tb_levels(tb)
-            msg_lines = traceback.format_exception(exctype, value, tb, length)
-        else:
-            msg_lines = traceback.format_exception(exctype, value, tb)
+        tb = self._clean_tracebacks(exctype, value, tb, test)
+        tb_e = traceback.TracebackException(
+            exctype, value, tb,
+            capture_locals=self.tb_locals, compact=True)
+        msgLines = list(tb_e.format())
 
         if self.buffer:
-            # Only try to get sys.stderr as it might not be
-            # StringIO yet, e.g. when test fails during __call__
-            try:
-                error = sys.stderr.getvalue()
-            except AttributeError:
-                error = None
+            output = sys.stdout.getvalue()
+            error = sys.stderr.getvalue()
+            if output:
+                if not output.endswith('\n'):
+                    output += '\n'
+                msgLines.append(STDOUT_LINE % output)
             if error:
                 if not error.endswith('\n'):
                     error += '\n'
-                msg_lines.append(error)
-        # This is the extra magic to make sure all lines are str
-        encoding = getattr(sys.stdout, 'encoding', 'utf-8')
-        lines = []
-        for line in msg_lines:
-            if not isinstance(line, str):
-                # utf8 shouldn't be hard-coded, but not sure f
-                line = line.encode(encoding)
-            lines.append(line)
-
-        return ''.join(lines)
+                msgLines.append(STDERR_LINE % error)
+        return ''.join(msgLines)
